95a165c34f2d74a18136afb5f56c0b7cf489f073
romanar2006 <romanar20061@gmail.com>
2024-09-18 22:37:39 +0300

    first

---
+++ Creator/Creator/Creator.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+void clearInput() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+
+    ofstream outFile(argv[1], ios::binary);
+    if (!outFile) {
+        cerr << "Файл не открывается\n";
+        return 1;
+    }
+
+    int numRecords;
+    try {
+        numRecords = stoi(argv[2]);
+        if (numRecords <= 0) throw invalid_argument("Неверное число");
+    }
+    catch (const exception& e) {
+        cerr << "Что-то не то: " << e.what() << "\n";
+        return 1;
+    }
+
+    vector<Employee> employees;
+    for (int i = 0; i < numRecords; ++i) {
+        Employee emp;
+
+        while (true) {
+            cout << "Введите номер сотрудника: ";
+            if (!(cin >> emp.num) || emp.num <= 0) {
+                cerr << "Что-то не то, нужно натуральное число\n";
+                clearInput();
+            }
+            else break;
+        }
+
+        cout << "Введите имя сотрудника: ";
+        cin >> emp.name;
+
+        while (true) {
+            cout << "Введите число отработанных часов: ";
+            if (!(cin >> emp.hours) || emp.hours < 0) {
+                cerr << "Что-то не то, нужно неотрицательное число\n";
+                clearInput();
+            }
+            else break;
+        }
+
+        employees.push_back(emp);
+    }
+
+    sort(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
+        return a.num < b.num;
+        });
+
+    for (const auto& emp : employees) {
+        outFile.write(reinterpret_cast<const char*>(&emp), sizeof(Employee));
+    }
+
+    outFile.close();
+    return 0;
+}
---
+++ Reporter/Reporter/Reporter.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+void clearInput() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+
+    ifstream inFile(argv[1], ios::binary);
+    if (!inFile) {
+        cerr << "Не открывается бинарный файл\n";
+        return 1;
+    }
+
+    ofstream outFile(argv[2]);
+    if (!outFile) {
+        cerr << "Не открывается файл отчета\n";
+        return 1;
+    }
+
+    double hourlyRate;
+    try {
+        hourlyRate = stod(argv[3]);
+        if (hourlyRate <= 0) throw invalid_argument("Неверная ставка");
+    }
+    catch (const exception& e) {
+        cerr << "Что-то не то: " << e.what() << "\n";
+        return 1;
+    }
+
+    vector<Employee> employees;
+    Employee emp;
+
+    while (inFile.read(reinterpret_cast<char*>(&emp), sizeof(Employee))) {
+        employees.push_back(emp);
+    }
+
+    sort(employees.begin(), employees.end(), [](const Employee& a, const Employee& b) {
+        return a.num < b.num;
+        });
+
+    outFile << "Отчет по файлу \"" << argv[1] << "\"\n";
+    outFile << left << setw(15) << "Номер"
+        << setw(20) << "Имя"
+        << setw(10) << "Часы"
+        << setw(15) << "Зарплата\n";
+
+    cout << left << setw(15) << "Номер"
+        << setw(20) << "Имя"
+        << setw(10) << "Часы"
+        << setw(15) << "Зарплата\n";
+
+    for (const auto& e : employees) {
+        double salary = e.hours * hourlyRate;
+        outFile << left << setw(15) << e.num
+            << setw(20) << e.name
+            << setw(10) << e.hours
+            << setw(15) << salary << "\n";
+
+        cout << left << setw(15) << e.num
+            << setw(20) << e.name
+            << setw(10) << e.hours
+            << setw(15) << salary << "\n";
+    }
+
+    inFile.close();
+    outFile.close();
+    return 0;
+}
